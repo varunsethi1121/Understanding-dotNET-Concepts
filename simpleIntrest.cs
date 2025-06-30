e5b615f5a33e9dd6e1edfeb573a1a676db746301
VarunSethi <varunsethi1121@gmail.com>
2025-06-30 13:08:25 +0530

    CommitFromLocal

---
+++ simpleIntrest.cs
@@ -0,0 +1,10 @@
+using System;
+class simpleIntrest{
+    public static void Main(){
+        int P = 10000;
+        int R = 12;
+        int T = 5;
+        int result = (P*R*T)/100;
+        System.Console.WriteLine("Calculated Simple Intrest on the given data is: " + result);
+    }
+}
