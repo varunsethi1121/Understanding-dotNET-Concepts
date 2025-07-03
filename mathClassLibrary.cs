bb814307b8dcbdd4dc638306c7a132e69c024696
VarunSethi <varunsethi1121@gmail.com>
2025-07-03 11:48:35 +0530

    CommitFromLocal

---
+++ mathClassLibrary.cs
@@ -0,0 +1,11 @@
+using System;
+class mathClassLibrary
+{
+    public static void Main()
+    {
+        int y1 = 99;
+        int y2 = 9;
+        int largestNumber = Math.Max(y1,y2);
+        Console.WriteLine(largestNumber);
+    }
+}
